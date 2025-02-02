2f23428ba2e03df597e690e60bd151d5029d6e79
odhyp <pradhana.odhy@gmail.com>
2025-02-02 17:27:50 +0700

    feat: add update_daftar_saham function in helper.py

--- src/helper.py
+++ src/helper.py
@@ -1,6 +1,13 @@
 from src.bot_idx import IDXBot
+from src.converter import convert_daftar_saham
+from src.utils import PathManager as pm
 
 
-def sample_func():
+def update_daftar_saham():
+    excel_path = pm.get_file_path("daftar_saham.xlsx")
+    json_path = pm.get_file_path("daftar_saham.json")
+
     with IDXBot() as bot:
-        bot.download_daftar_saham(file_name="daftar_saham.xlsx")
+        bot.download_daftar_saham(file_name=excel_path)
+
+    convert_daftar_saham(input_file=excel_path, output_file=json_path)
