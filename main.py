db6f491c67c1608dfe9816cc76fefbf5b3025e47
odhyp <pradhana.odhy@gmail.com>
2025-02-02 20:45:49 +0700

    feat: update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from src.helper import update_daftar_saham
+from src.data_reader import read_json
+from src.helper import update_daftar_saham, ringkasan_saham
 
 
 def main():
-    update_daftar_saham()
+    # update_daftar_saham()
+    ringkasan_saham()
 
 
 if __name__ == "__main__":
