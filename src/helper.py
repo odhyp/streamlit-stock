cc9f9f92319ed822197d9c17851ea605d0d0d03b
odhyp <pradhana.odhy@gmail.com>
2025-02-02 20:45:03 +0700

    feat: update helper.py with ringkasan_saham method

--- src/helper.py
+++ src/helper.py
@@ -1,9 +1,11 @@
 from src.bot_idx import IDXBot
 from src.converter import convert_daftar_saham
-from src.utils import PathManager as pm
+from src.utils import PathManager as pm, generate_date_range
 
 
 def update_daftar_saham():
+    # TODO: add a logger file to know when the "daftar_saham" is last updated
+    # TODO: delete unused "daftar_saham.xlsx" file after converting
     excel_path = pm.get_file_path("daftar_saham.xlsx")
     json_path = pm.get_file_path("daftar_saham.json")
 
@@ -11,3 +13,14 @@ def update_daftar_saham():
         bot.download_daftar_saham(output_name=excel_path)
 
     convert_daftar_saham(input_file=excel_path, output_file=json_path)
+
+
+def ringkasan_saham():
+    start_date = "2025-01-02"
+    end_date = "2025-01-02"
+
+    output_dir = pm.get_output_path("ringkasan_saham")
+    date_range = generate_date_range(start_date, end_date)
+
+    with IDXBot() as bot:
+        bot.download_ringkasan_saham(output_dir, date_range)
