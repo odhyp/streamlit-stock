92070d43cf7d95479fe9d1108b9910318742ccf2
odhyp <pradhana.odhy@gmail.com>
2025-02-02 14:10:48 +0700

    feat: add main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from src.helper import sample_func
+from src.converter import convert_daftar_saham
+
+
+def main():
+    # sample_func()
+    convert_daftar_saham
+
+
+if __name__ == "__main__":
+    main()
