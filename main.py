0a224d20fbad5fceeba819ea93ea02c67b269d83
odhyp <pradhana.odhy@gmail.com>
2025-02-02 17:31:14 +0700

    refactor: update function name

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from src.helper import sample_func
-from src.converter import convert_daftar_saham
+from src.helper import update_daftar_saham
 
 
 def main():
-    # sample_func()
-    convert_daftar_saham
+    update_daftar_saham()
 
 
 if __name__ == "__main__":
